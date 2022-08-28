50813b4e407c9cf21857782ce25ce4b3ff1ce446
David Bustos Usta <66847915+dfbustosus@users.noreply.github.com>
2022-08-28 14:43:58 -0400

    Create Lectura_txt.py

---
+++ Data Engineering - Github/Semana 5/Lectura_txt.py
@@ -0,0 +1,12 @@
+# De ser necesario usar colab
+from google.colab import drive
+import os
+drive.mount('/content/gdrive')
+# Cambiar ruta
+%cd '/content/gdrive/MyDrive'
+# Lectura de archivo
+df= pd.read_csv('pokemon_data.txt',delimiter='\t')
+# Mostrar ultimas 5 filas
+df.tail()
+# Mostrar ciertas columnas
+print(df[['Name','Type 1','HP','Attack','Defense']].head())
