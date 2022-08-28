7fce7d1d2946e6d973262f5f2534b7c6a716a6ec
David Bustos Usta <66847915+dfbustosus@users.noreply.github.com>
2022-08-28 14:45:37 -0400

    Create Lectura_xlsx.py

---
+++ Data Engineering - Github/Semana 5/Lectura_xlsx.py
@@ -0,0 +1,12 @@
+# De ser necesario usar colab
+from google.colab import drive
+import os
+drive.mount('/content/gdrive')
+# Cambiar ruta de acceso
+%cd '/content/gdrive/MyDrive'
+# Lectura de archivo
+df= pd.read_excel('defaultoutput.xlsx')
+# Mostrar las priemra 5 filas
+df.head()
+# Elegir columnas de interés
+print(df[['index','ID','Year_Birth','Education','Income']].head())
