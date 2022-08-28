af42380a6a55ea273fc391bf6be99a508a48d43e
David Bustos Usta <66847915+dfbustosus@users.noreply.github.com>
2022-08-28 14:48:12 -0400

    Update Lectura_xlsx.py

--- Data Engineering - Github/Semana 5/Lectura_xlsx.py
+++ Data Engineering - Github/Semana 5/Lectura_xlsx.py
@@ -4,6 +4,7 @@ import os
 drive.mount('/content/gdrive')
 # Cambiar ruta de acceso
 %cd '/content/gdrive/MyDrive'
+import pandas as pd
 # Lectura de archivo
 df= pd.read_excel('defaultoutput.xlsx')
 # Mostrar las priemra 5 filas
